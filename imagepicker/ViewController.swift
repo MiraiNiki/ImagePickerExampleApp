03ed6b67f058aed2b03f3a7706c3b6ab35d366f4
Niki Mirai <s1411400@coins.tsukuba.ac.jp>
2017-09-21 21:42:21 +0900

    get picture from photolibrary

--- imagepicker/ViewController.swift
+++ imagepicker/ViewController.swift
@@ -8,11 +8,19 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, UIImagePickerControllerDelegate,
+    UINavigationControllerDelegate {
 
+    @IBOutlet weak var photoImageView: UIImageView!
+    @IBOutlet weak var label: UILabel!
+    // pickerのインスタンスを作成
+    let picker = UIImagePickerController()
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        // pickerのdelegate先を
+        picker.delegate = self
+
     }
 
     override func didReceiveMemoryWarning() {
@@ -20,6 +28,25 @@ class ViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
 
+    func imagePickerController(_ picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [String : AnyObject]) {
+            let chosenImage = info[UIImagePickerControllerOriginalImage] as! UIImage //2
+            photoImageView.contentMode = .scaleAspectFit //3
+            photoImageView.image = chosenImage //4
+            dismiss(animated: true, completion: nil) //5
+    }
+
+    func imagePickerControllerDidCancel(picker: UIImagePickerController) {
+        dismiss(animated: true, completion: nil)
+    }
+
+    @IBAction func picButtonPushed(_ sender: Any) {
+        let album = UIImagePickerControllerSourceType.photoLibrary
+        if UIImagePickerController.isSourceTypeAvailable(album) {
+            picker.sourceType = UIImagePickerControllerSourceType.photoLibrary
+            picker.allowsEditing = true
+            self.present(picker, animated: true, completion: nil)
+        }
+    }
 
 }
 
